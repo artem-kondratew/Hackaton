af7fb25cad99ebbb8736aeb919eb48ac8a0f6400
AntonPolyTech <COOLgay123@mail.ru>
2023-04-22 14:46:57 +0300

    Claw added

---
+++ Arduino/Claw.h
@@ -0,0 +1,40 @@
+#ifndef Claw_h
+#define Claw_h
+
+#include <Servo.h>
+
+
+class Claw{
+    Servo Pin_D9; // Claw mover
+    Servo Pin_D8;  // Claw
+public:
+
+    void ServoAttach(){
+        Pin_D9.attach(9);
+        Pin_D8.attach(8);
+    }
+
+    void Move(int Angle){
+        if(Angle > 180) Angle = 180;
+        if(Angle < 100) Angle = 100;
+        Pin_D9.write(Angle);
+    }
+
+    void Catch(int Angle){
+        if(Angle > 180) Angle = 180;
+        if(Angle < 0) Angle = 0;
+
+        Pin_D8.write(Angle);
+    }
+
+    void ServoSetUp(){
+        ServoAttach();
+        Move(100);
+        Catch(0);
+    }
+
+};
+
+
+#endif
+
