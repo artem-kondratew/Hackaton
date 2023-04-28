440f41e36a163ff6454601a261758219bcb61cb9
artem-kondratew <artemkondratev5@gmail.com>
2023-04-28 18:36:02 +0300

    Vision added

---
+++ App/Vision.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 4/28/23.
+//
+
+#include "Vision.h"
---
+++ App/Vision.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 4/28/23.
+//
+
+#ifndef HACKATON_VISION_H
+#define HACKATON_VISION_H
+
+
+class Vision {
+
+};
+
+
+#endif //HACKATON_VISION_H
