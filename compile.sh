c9852fdea9a2e52cc30b07ddf987a38a44bdab72
artem-kondratew <artemkondratev5@gmail.com>
2024-04-22 13:40:57 +0300

    unused

--- compile.sh
+++
@@ -1,1 +0,0 @@
-g++ App/Connect.cpp  App/Connect.h  App/Exception.h  App/Gservo.cpp  App/Gservo.h  App/History.h  App/List.h App/graphics.h  App/main.cpp  App/str.cpp  App/str.h -lncurses -std=c++2a -o connect
