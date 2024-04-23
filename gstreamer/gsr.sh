490c231965da737a6e17a3acadcb677e061ea6ec
artem-kondratew <artemkondratev5@gmail.com>
2024-04-23 19:23:12 +0300

    rm unused

--- gstreamer/gsr.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 udpsrc caps="application/x-rtp,media=(string)video,encoding-name=(string)JPEG" port=4000 ! rtpjpegdepay ! jpegdec ! queue ! autovideosink
--- gstreamer/gsv.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 v4l2src device=/dev/video0 ! 'image/jpeg,width=640,height=480, framerate=10/1' ! rtpjpegpay ! udpsink host= port=4000 sync=false
