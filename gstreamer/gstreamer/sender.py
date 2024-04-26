92a0affe990b3db3d6f37d1a39aedf062b669643
Artem Kondratev <112778404+artem-kondratew@users.noreply.github.com>
2024-04-26 13:27:14 +0300

    Update sender.py

--- gstreamer/gstreamer/sender.py
+++ gstreamer/gstreamer/sender.py
@@ -22,4 +22,3 @@ def main():
     gs = f'gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw, format=I420, width=320, height=240, framerate=30/1 ! rtpvrawpay ! udpsink host={ip} port=5000'
     
     subprocess.run(gs, shell=True, stdin=subprocess.PIPE)
-
